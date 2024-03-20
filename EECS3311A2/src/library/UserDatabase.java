e402474e1ce41569c6537673cbc77a4fe5f656a7
JibrilOsman03 <98564145+JibrilOsman03@users.noreply.github.com>
2024-03-19 21:14:37 -0400

    Update UserDatabase.java

--- EECS3311A2/src/library/UserDatabase.java
+++ EECS3311A2/src/library/UserDatabase.java
@@ -3,7 +3,11 @@ package library;
 import java.util.List;
 
 public interface UserDatabase {
-
+	
+	//allows only 1 instance of UserDatabase, singleton pattern
+	public static UserDatabase getInstance() {
+		return null;
+	}
 	public void update() throws Exception;
 	public User getRegisteredUserByEmail(String email) throws Exception;
 	void addUser(User user);
