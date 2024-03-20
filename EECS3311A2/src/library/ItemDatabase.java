47cc18f95e1d0fbb768570d0705ca31b67dee15c
JibrilOsman03 <98564145+JibrilOsman03@users.noreply.github.com>
2024-03-19 21:13:10 -0400

    Update ItemDatabase.java

--- EECS3311A2/src/library/ItemDatabase.java
+++ EECS3311A2/src/library/ItemDatabase.java
@@ -3,7 +3,12 @@ package library;
 import java.util.List;
 
 public interface ItemDatabase {
-
+	
+	//allows only 1 instance of ItemDatabase, singleton pattern
+	public static ItemDatabase getInstance() {
+		return null;
+	}
+	
 	void load(String path) throws Exception;
 
 	void update() throws Exception;
