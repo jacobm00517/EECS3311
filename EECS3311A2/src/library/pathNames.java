681fe31e74fd2dc0d5bf95e04a6ed46b6cf8c0b8
Ishi <113114714+ishapannu@users.noreply.github.com>
2024-03-19 23:41:05 -0400

    Delete EECS3311A2 directory

--- EECS3311A2/src/library/pathNames.java
+++
@@ -1,7 +0,0 @@
-package library;
-
-public class pathNames {
-	public static final String path ="./user.csv";
-	public static final String unregisteredDBPath = "./URuser.csv";
-	public static String itemDBpath = "./Items.csv";
-}
