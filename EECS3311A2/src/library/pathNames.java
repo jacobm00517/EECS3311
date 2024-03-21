ee94cada2eefb063ef3c52761a25f3ca31c83532
Ishi <113114714+ishapannu@users.noreply.github.com>
2024-03-21 02:33:04 -0400

    Add files via upload

--- EECS3311A2/src/library/pathNames.java
+++ EECS3311A2/src/library/pathNames.java
@@ -3,5 +3,8 @@ package library;
 public class pathNames {
 	public static final String path ="./user.csv";
 	public static final String unregisteredDBPath = "./URuser.csv";
-	public static String itemDBpath = "./Items.csv";
+	public static final String itemDBpath = "./Items.csv";
+	
+	public static final String subPath = "./Subscription.csv";
+	
 }
